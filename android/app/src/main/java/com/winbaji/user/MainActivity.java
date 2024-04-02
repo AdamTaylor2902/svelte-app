2367c58a6de4678f274201776e2a6526c5147977
Adam Taylor <adamtaylor2902@gmail.com>
2024-04-02 17:38:55 +0530

    Initial commit

---
+++ android/app/src/main/java/com/winbaji/user/MainActivity.java
@@ -0,0 +1,5 @@
+package com.winbaji.user;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.winbaji.user',
+	appName: 'Winbaji',
+	webDir: '',
+	server: {
+		androidScheme: 'https'
+	}
+};
+
+export default config;
